2d6891e511ebe7fe7b103c1ee5cbdf6fcb3c1aa4
Kunal-Rawat <278743100+kunalrawat-cse@users.noreply.github.com>
2026-09-04 17:45:11 +0530

    basic c program(pattern)

---
+++ pattern4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    for (int i=5; i>=1; i--){
+          printf("\n");
+
+        for(int j=i; j<=5; j++){
+            printf("%d", j);
+             
+            
+
+        }
+      
+    }
+
+    return 0;
+}
